b181f41a8ecca301084293c182e356d560496169
Leonardo Guedes Ferreira <leoguedes99@gmail.com>
2022-05-30 15:56:51 -0300

    Adicionar contador próximo ao carrinho

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -9,13 +9,26 @@ export default class Product extends React.Component {
     pictures: [],
     cartProducts: [],
     userReview: [],
+    productsCount: 0,
   }
 
   componentDidMount = async () => {
     if (!localStorage.getItem('userReview')) localStorage.setItem('userReview', '[]');
     const { match: { params: { id } } } = this.props;
     this.getUserReviewFromLs();
+    this.getNumberInCart();
     await this.getProductById(id);
+  }
+
+  getNumberInCart() {
+    const getFromLocalStorage = JSON.parse(localStorage.getItem('cart'));
+    const products = getFromLocalStorage.map((item) => item.id);
+    let soma = 0;
+    products.forEach((product) => {
+      const number = Number(localStorage.getItem(product));
+      soma += number;
+    });
+    this.setState({ productsCount: soma });
   }
 
   getUserReviewFromLs = () => {
@@ -51,6 +64,7 @@ export default class Product extends React.Component {
         localStorage.setItem('cart', JSON.stringify(newArrayProducts));
         localStorage.setItem(product.id, 1);
       }
+      this.getNumberInCart();
     });
   }
 
@@ -83,8 +97,12 @@ export default class Product extends React.Component {
   render() {
     const { product: { title, price }, pictures } = this.state;
     const { product, userReview } = this.state;
+    const { productsCount } = this.state;
     return (
       <div>
+        <span data-testid="shopping-cart-size">
+          { productsCount }
+        </span>
         <Link
           to="/cart"
         >
